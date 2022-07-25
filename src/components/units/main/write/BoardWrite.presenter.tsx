fa1470f57cadb3f0e03937f9bb1629711d15d713
RumbleBi <olzlel2000@gmail.com>
2022-07-25 18:06:26 +0900

    toastEditer & mock-up

--- src/components/units/main/write/BoardWrite.presenter.tsx
+++ src/components/units/main/write/BoardWrite.presenter.tsx
@@ -1,9 +1,41 @@
 import * as C from "./BoardWrite.styles";
+import { Editor } from "@toast-ui/react-editor";
+import "@toast-ui/editor/dist/toastui-editor.css";
 
-export default function BoardWriteUI() {
+export default function BoardWriteUI(props: any) {
   return (
     <C.Wrapper>
-      <div></div>
+      <C.TitleInput type="text" placeholder="제목을 입력하세요" />
+      <C.TagsInput type="text" placeholder="태그를 입력하세요" />
+      <C.TagsWrapper>
+        <C.Tags>태그1qewef</C.Tags>
+      </C.TagsWrapper>
+      <Editor
+        // placeholder="내용을 입력해주세요"
+        useCommandShortcut={false} // 키보드 입력 컨트롤 방지
+        initialValue="당신의 이야기를 적어보세요..."
+        previewStyle="tab" // 미리보기 스타일 지정
+        minHeight="500px" // 에디터 창 높이
+        initialEditType="wysiwyg" // 초기 입력모드 설정(디폴트 markdown)
+        toolbarItems={[
+          // 툴바 옵션 설정
+          [
+            "heading",
+            "bold",
+            "italic",
+            "strike",
+            "hr",
+            "quote",
+            "ul",
+            "ol",
+            "task",
+            "indent",
+            "outdent",
+            "link",
+            "code",
+          ],
+        ]}
+      ></Editor>
     </C.Wrapper>
   );
 }
--- src/components/units/main/write/BoardWrite.styles.ts
+++ src/components/units/main/write/BoardWrite.styles.ts
@@ -3,9 +3,51 @@ import styled from "@emotion/styled";
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-evenly;
+
   align-items: center;
   background-color: #121212;
   height: 100%;
   width: 100%;
+`;
+export const TitleInput = styled.input`
+  width: 800px;
+  height: 50px;
+  margin-top: 200px;
+  font-size: 2rem;
+  color: white;
+  font-weight: bold;
+  ::placeholder {
+    color: #ececec;
+  }
+  margin-bottom: 30px;
+  :focus {
+    border-bottom: 1px solid #eaeaea;
+  }
+`;
+export const TagsInput = styled.input`
+  width: 800px;
+  height: 30px;
+  color: white;
+  font-size: 1.25rem;
+  font-weight: bold;
+  ::placeholder {
+    color: #ececec;
+  }
+  :focus {
+    border-bottom: 1px solid #eaeaea;
+  }
+`;
+export const TagsWrapper = styled.div`
+  margin-top: 20px;
+  margin-bottom: 40px;
+  display: flex;
+  align-items: center;
+  width: 800px;
+  height: 50px;
+`;
+export const Tags = styled.span`
+  color: #62e6be;
+  background-color: #202020;
+  border-radius: 50px;
+  padding: 10px 10px;
 `;
